a3c99a36042f274eada487515d01f25715d25ebe
Amna <amnah.mohammed.eam.js@tuwaiq.edu.sa>
2021-11-30 11:02:09 +0300

    Quize

---
+++ app.js
@@ -0,0 +1,37 @@
+
+var food = ["burger","kabsa","spaghetti","pizza"];
+localStorage.setItem("foods", JSON.stringify(food)); 
+var storedColors = JSON.parse(localStorage.getItem("foods")); 
+
+// 2
+
+
+
+const cars = {merceds:{country:"germany", model:[2000]},toyota:{country:"japan", model:[2000]},
+    ford:{country:"USA", model:[2000]}};
+
+
+///3
+    
+function getCar(car) {
+    return console.log(`${2000} is made by ${car} in ${japsn}`); }
+    
+    
+        
+//4
+
+const addFood = function(meal){
+    localStorage.setItem('addFood' ,JSON.stringify(addFood));
+    
+    }
+
+    class Dog {
+        
+            makeSound() {
+            System.out.println('5',"woof");
+        }
+    }
+
+//5
+
+
