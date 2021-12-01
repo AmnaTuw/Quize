4ff3c4bc105127d21d8d73a8def28f3869fec53c
Amna <amnah.mohammed.eam.js@tuwaiq.edu.sa>
2021-12-01 09:00:00 +0300

    Quize

--- app.js
+++ app.js
@@ -20,18 +20,68 @@ function getCar(car) {
         
 //4
 
-const addFood = function(meal){
-    localStorage.setItem('addFood' ,JSON.stringify(addFood));
-    
-    }
-
-    class Dog {
-        
-            makeSound() {
-            System.out.println('5',"woof");
-        }
-    }
-
+const addFood = function (meal) {
+    const Meals = JSON.parse(sessionStorage.getItem("food"));
+    Meals.push(meal);
+    sessionStorage.setItem("food", JSON.stringify(Meals));
+};
+const dog = new Dog("Spike","dobberman",5,);
+console.log(user.getDog(),dog.getspecies(),dog.getage());
 //5
+class Dog {
+    constructor(name, species,age,sound) {
+        this.name = name;
+        this.species = species;
+        this.age=age;
+                this.sound=sound;
+            }
+        getDog(){
+            return this.name
+        }
+        getspecies(){
+            return this.species
+        }
+        getage(){
+            return this.age
+        }
+        makeSound(){
+            return this.sound
+        }}
+        //6
+        export default class Home extends Component {
+            constructor(props) {
+            super();
+            this.state = {
+                a: "",
+                a: "",
+            };
+            }
+        
+            componentDidMount() {
+            this.setState({
+                a: "welcome to",
+            });
+            }
+        
+            componentDidUpdate(prevProps, prevState) {
+            if (prevState.a === this.state.a) {
+                this.setState({
+                message2: "quiz awesome :)",
+                });
+            }
+            }
+        
+            render() {
+            return (
+                <div>
+                <h1>{this.state.a}</h1>
+                <h1>{this.state.aa}</h1>
+                </div>
+            );
+            }
+        }
+
+
+
 
 
